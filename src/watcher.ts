481e91a52bee71969c42bf46043fa8033dbc9168
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-12-21 16:33:37 +0300

    wip

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,7 +1,7 @@
 export function reset(watcher: WatcherBase): Watcher {
     watcher.match = watcher.pattern.slice(0)
     watcher.matchIndex = 0
-    return <Watcher>watcher
+    return <Watcher > watcher
 }
 
 export function consume(watcher: Watcher, chunk: Data): boolean {
@@ -23,8 +23,8 @@ export function consume(watcher: Watcher, chunk: Data): boolean {
             if (!expected) {
                 watcher.match[watcher.matchIndex] = chunk[chunkIndex]
             }
-            
-            if(expected === chunk[chunkIndex]) {
+
+            if (expected === chunk[chunkIndex]) {
                 watcher.matchIndex++
 
                 if (watcher.matchIndex === watcher.match.length) {
@@ -46,28 +46,28 @@ export function consume(watcher: Watcher, chunk: Data): boolean {
 
 export function toConsumable(pattern: Pattern): ConsumablePattern {
     let consumablePattern: ConsumablePattern = []
-  
+
     for (let index = 0; index < pattern.length; index++) {
-      const sample = pattern[index]
-  
-      if (Array.isArray(sample)) {
-        consumablePattern = consumablePattern.concat(sample)
-      } else {
-        consumablePattern.push(sample)
-      }
+        const sample = pattern[index]
+
+        if (Array.isArray(sample)) {
+            consumablePattern = consumablePattern.concat(sample)
+        } else {
+            consumablePattern.push(sample)
+        }
     }
-  
+
     for (let index in consumablePattern) {
         const sample = consumablePattern[index]
         if (!(!isNaN(sample) || sample instanceof Function || sample === undefined)) {
             throw new TypeError('Cannot create pattern with "' + typeof sample + '" sample type')
         }
     }
-  
-    return consumablePattern
-  }
 
-  export function create(pattern: Pattern, callback?: Function): Watcher {
+    return consumablePattern
+}
+
+export function create(pattern: Pattern, callback ? : Function): Watcher {
     const watcher: WatcherBase = {
         pattern: toConsumable(pattern),
         callback
