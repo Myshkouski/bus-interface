a50debd776b4fb21f6d249a1e7ee69de4f563eb3
Alexei <alexeimyshkouski@gmail.com>
2019-11-30 23:40:08 +0300

    wip

--- bus.mjs
+++ bus.mjs
@@ -1,20 +1,23 @@
 import { Duplex } from 'stream'
 
 function Route() {
+	Duplex.call(this, {
+		read() {},
+		async write(chunk, encoding, cb) {
+			try {
+				await this.consume(chunk, encoding, cb)
+			} catch (error) {
+				this.emit('error', error)
+			}
+		}
+	})
+
 	this._filters = []
 }
 
 Route.prototype = Object.create(Duplex.prototype)
 Object.assign(Route.prototype, {
 	constructor: Route,
-
-	async _write(chunk, encoding, cb) {
-		try {
-			await this.consume(chunk, encoding, cb)
-		} catch (error) {
-			this.emit('error', error)
-		}
-	},
 
 	use(handler) {
 		this._filters.push(handler)
@@ -36,6 +39,31 @@ Object.assign(Route.prototype, {
 })
 
 function Bus() {
+	Duplex.call(this, {
+		read() {},
+		async write(chunk, encoding, cb) {
+			for (let index = 0; chunk.length && index < this._routes.length;) {
+				const [id, route] = this._routes[index]
+				let consumed = 0
+
+				try {
+					consumed = await route.consume(chunk, encoding)
+				} catch (error) {
+					this.emit('error', error)
+				}
+
+				if (consumed) {
+					chunk = chunk.slice(consumed)
+					index = 0
+				} else {
+					index++
+				}
+			}
+
+			cb()
+		}
+	})
+	
 	this._routes = []
 }
 
@@ -48,28 +76,6 @@ Object.assign(Bus.prototype, {
 		//   chunk = this._sending.shift()
 		//   push = this.push(chunk)
 		// }
-	},
-
-	async _write(chunk, encoding, cb) {
-		for (let index = 0; chunk.length && index < this._routes.length;) {
-			const [id, route] = this._routes[index]
-			let consumed = 0
-
-			try {
-				consumed = await route.consume(chunk, encoding)
-			} catch (error) {
-				this.emit('error', error)
-			}
-
-			if (consumed) {
-				chunk = chunk.slice(consumed)
-				index = 0
-			} else {
-				index++
-			}
-		}
-
-		cb()
 	},
 
 	route(id, ...filters) {
